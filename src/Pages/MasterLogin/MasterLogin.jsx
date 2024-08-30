90489b4e640458e23ead9f270651c97639ddc530
zhumail88 <shumailtassadaq3@gmail.com>
2024-08-30 10:45:20 +0500

    Master Login done

--- src/Pages/MasterLogin/MasterLogin.jsx
+++ src/Pages/MasterLogin/MasterLogin.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import './MasterLogin.css'
+import InsidersLogo from '../../assets/masterLoginLogo.png'
+import Heading from '../../Components/Heading/Heading'
+import GreenButton from '../../Components/GreenButton/GreenButton'
+import BackLogo from '../../assets/backlogo.png'
 
 export default function MasterLogin() {
-  return (
-    <div>MasterLogin</div>
-  )
+    return (
+        <>
+            <div className="masterLogin">
+                <div className="ml-heading">
+                    <img src={InsidersLogo} alt="" />
+                    <Heading />
+                </div>
+
+                <div className="ml-login">
+                    <p>Administration Login</p>
+
+                    <div className="ml-input">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" />
+
+                        <label htmlFor="password">Password</label>
+                        <input type="password" />
+
+                    </div>
+
+                    <div className="ml-checkbox-fp">
+                        <div className="ml-checkbox">
+                            <input type="checkbox" />
+                            <label htmlFor="rememberMe">Remember Me</label>
+
+                        </div>
+
+                        <div className="forgetPassword">
+                            <li>Forget Password?</li>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="ml-btn">
+                        <GreenButton
+                            text='Sign In'
+                        />
+                    </div>
+
+                </div>
+
+                <div className="ml-backBtn">
+                    <img src={BackLogo} alt="" />
+                    <li>
+                        Back To Insider's Inventory
+                    </li>
+                </div>
+            </div>
+        </>
+    )
 }
